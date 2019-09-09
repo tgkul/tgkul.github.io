18fc0b721e9678d4cff746f2d5626c399fe25f47
Tushar Kulkarni <tkul@outlook.com>
2019-09-08 19:46:53 -0700

    React App

---
+++ precache-manifest.2040ccf6e23da07a40b912f48f72b8cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab9ec8100c62a25a98a2e1845a5ceb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8ad769f2b292b2a57c1",
+    "url": "/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "fa2b6c9f1920d62abbcf",
+    "url": "/static/js/2.2191e043.chunk.js"
+  },
+  {
+    "revision": "c8ad769f2b292b2a57c1",
+    "url": "/static/js/main.9b561705.chunk.js"
+  },
+  {
+    "revision": "74fa93c26ae9602f3ea8",
+    "url": "/static/js/runtime~main.6d08190d.js"
+  }
+]);
